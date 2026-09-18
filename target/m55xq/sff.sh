51f0e68025eb62b51fa3d8d38d5514bbbe49338d
console-ramoops <4ratiksharma@gmail.com>
2026-09-18 20:10:27 +0530

    m55xq: init

---
+++ target/m55xq/sff.sh
@@ -0,0 +1,7 @@
+# Copyright (c) 2026 console-ramoops
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Galaxy M55s 5G (m55xq)
+
+# Enable seamless refresh rate feature
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
